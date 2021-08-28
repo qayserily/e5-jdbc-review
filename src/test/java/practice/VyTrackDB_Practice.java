c6d80a1213b75088ecc987aa2ca1c17c85ba3c9d
qayserily <74465236+qayserily@users.noreply.github.com>
2021-08-28 21:30:02 +0300

    added configuration.properties and ConfigurationReader

--- src/test/java/practice/VyTrackDB_Practice.java
+++ src/test/java/practice/VyTrackDB_Practice.java
@@ -4,9 +4,9 @@ import utilities.DBUtils;
 
 public class VyTrackDB_Practice {
     public static void main(String[] args) {
-        String jdbc_url ="jdbc:mysql://3.87.155.124:3306/bitnami_orocrm";
-        String username="qa_user";
-        String password="qa_user";
+        String jdbc_url = "jdbc:mysql://3.87.155.124:3306/bitnami_orocrm";
+        String username = "qa_user" ;
+        String password = "qa_user" ;
 
         DBUtils.createConnection(jdbc_url,username,password);
 
